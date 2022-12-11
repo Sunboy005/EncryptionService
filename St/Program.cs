d09fd2fded4949c38be3aa3beafdbf4332b88ff5
Amos <sunboy005@github.com>
2022-12-11 13:33:43 +0100

    Encyption Service completed

---
+++ St/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using St;
+
+var path = "../../../PublicKey.txt";
+var content = File.ReadAllText(path);
+var path2 = "../../../PrivateKey.txt";
+var privateKey = File.ReadAllText(path2);
+
+Console.WriteLine("-------######## DataToEncrpyt= 'Hello Amos' ########---------------");
+
+var encryptedData = RsaEncry.Encrypt("Hello Amos", content);
+Console.WriteLine(encryptedData);
+
+Console.WriteLine("-------######## Decrypt The encrypted Data ########---------------");
+
+var decryptedData = RsaEncry.Decrypt(encryptedData, privateKey);
+Console.WriteLine(decryptedData);
